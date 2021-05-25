0c64a60588a4c805d0370c39d89c74ad5260be7e
Adam Weeks <aoweeks@gmail.com>
2021-05-25 22:24:08 +0100

    Add Three typings

--- src/app/services/three.service.ts
+++ src/app/services/three.service.ts
@@ -7,10 +7,42 @@ import * as THREE from 'Three';
 })
 export class ThreeService {
 
+  canvas: HTMLElement;
+  scene: THREE.Scene;
+  viewportSizes = {
+    height: new Number,
+    width: new Number
+  };
+  orthoganalCamera: boolean;
+
   constructor() { }
 
 
   public setCanvas(canvas: HTMLElement) {
-    console.log(canvas);
+
+    this.canvas = canvas;
+
+    // Get screen dimensions
+    this.viewportSizes.height = window.innerHeight; 
+    this.viewportSizes.width = window.innerWidth;
+    console.log(this.viewportSizes, window.innerWidth);
+
+
+  }
+
+  
+  private initialSetup() {
+
+    /**
+     * Scene Setup
+     */
+
+    this.scene = new THREE.Scene();
+
+    if (this.orthoganalCamera) {
+
+    } else {
+      const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    }
   }
 }

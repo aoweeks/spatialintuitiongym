337beffe0f9f51e495dc8cbea083b179fc52f573
Adam Weeks <aoweeks@gmail.com>
2021-06-10 16:55:17 +0100

    Functions to draw line on 2D canvas with mouse

--- src/app/exercises/2d-canvases/base-2d-canvas.component.ts
+++ src/app/exercises/2d-canvases/base-2d-canvas.component.ts
@@ -11,6 +11,9 @@ export class Base2dCanvasComponent extends BaseCanvasComponent implements AfterV
   @ViewChild('2dCanvas') canvasRef: ElementRef;
   private context;
 
+  private mouseDownPos = { x: 0, y : 0};
+  private mouseIsDown = false;
+
   ngAfterViewInit() {
     this.context = this.canvasRef.nativeElement.getContext('2d');
     super.ngAfterViewInit();
@@ -26,16 +29,38 @@ export class Base2dCanvasComponent extends BaseCanvasComponent implements AfterV
    *  Mouse Event Handlers
    */
 
-  public mouseMove(event: Event) {
-    console.log(event);
+  public mouseDown(event: MouseEvent) {
+    this.context.restore();
+
+    this.mouseDownPos.x = event.clientX;
+    this.mouseDownPos.y = event.clientY;
+
+    this.mouseIsDown = true;
   }
 
-  public mouseDown(event: Event) {
-    console.log('click');
+  public mouseMove(event: MouseEvent) {
+
+    if(this.mouseIsDown) {
+
+      this.context.clearRect(
+        0,
+        0,
+        this.canvasRef.nativeElement.width,
+        this.canvasRef.nativeElement.height
+      );
+      this.context.beginPath();
+
+      this.context.lineWidth = 5;
+
+      this.context.moveTo(this.mouseDownPos.x, this.mouseDownPos.y);
+      this.context.lineTo(event.clientX, event.clientY);
+      this.context.stroke();
+    }
   }
 
-  public mouseUp(event: Event) {
-    console.log('unclick');
+  public mouseUp(event: MouseEvent) {
+    this.mouseIsDown = false;
+    this.context.save();
   }
 
 }

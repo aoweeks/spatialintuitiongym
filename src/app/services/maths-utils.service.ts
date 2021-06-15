05c255d69b4e1acd457eec861c017bbbdc7c2c65
Adam Weeks <aoweeks@gmail.com>
2021-06-15 23:17:59 +0100

    Return line object

--- src/app/services/maths-utils.service.ts
+++ src/app/services/maths-utils.service.ts
@@ -9,5 +9,9 @@ export class MathsUtilsService {
 
   public getLineFromPoints(point1, point2) {
     const slope = (point1.x - point2.x) / (point1.y - point2.y);
+    const intercept = 0;
+
+    const line = { slope, intercept };
+    return line;
   }
 }

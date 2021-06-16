3c203da95c468c0d9bdf6fa1bf7f78b4c5daa2aa
Adam Weeks <aoweeks@gmail.com>
2021-06-16 21:13:53 +0100

    Begin closest point on line function

--- src/app/services/maths-utils.service.ts
+++ src/app/services/maths-utils.service.ts
@@ -9,9 +9,17 @@ export class MathsUtilsService {
 
   public getLineFromPoints(point1, point2) {
     const slope = (point1.x - point2.x) / (point1.y - point2.y);
-    const intercept = 0;
+    const intercept = point1.y - (point1.x * slope);
 
     const line = { slope, intercept };
     return line;
+  }
+
+  public closestPointOnLine(point, line) {
+  /**
+   * Get slope-intercept of perpendicular line by applying inverse slope to point.
+   */
+    const closestPoint = {x: 0, y: 0};
+    return closestPoint;
   }
 }

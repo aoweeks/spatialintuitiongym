26eda8c1fdffd8cfac160e24488530c63ebab081
Adam Weeks <aoweeks@gmail.com>
2021-06-17 14:41:30 +0100

    Handle vertical/horizontal lines

--- src/app/services/maths-utils.service.ts
+++ src/app/services/maths-utils.service.ts
@@ -5,30 +5,50 @@ import { Injectable } from '@angular/core';
 })
 export class MathsUtilsService {
 
-  constructor() {console.log(this.getLineFromPoints({x: 1, y: 4}, {x: 2, y: 8}))};
+  constructor() {console.log(this.getLineFromPoints({x: 1, y: 4}, {x: 1, y: 8}));};
 
   public getLineFromPoints(point1, point2) {
     const slope = (point1.y - point2.y) / (point1.x - point2.x);
     const intercept = this.calculateIntercept(slope, point1);
 
-    const line = { slope, intercept };
+    let x = null;
+    let y = null;
+
+    if(slope === -Infinity) {
+      x = point1.x;
+    } else if (slope === 0 ) {
+      y = point1.y;
+    }
+    const line = { slope, intercept, x, y };
     return line;
   }
 
   //! Need to handle horizontal and vertical lines
   public closestPointOnLine(point, line) {
 
+    let x;
+    let y;
 
-    const perpSlope = (1 / line.slope) * -1;
-    const perpIntercept = this.calculateIntercept(perpSlope, point);
-    const perpLine = { slope: perpSlope, intercept: perpIntercept };
+    if(line.slope === -Infinity) {
+      x = line.x;
+      y = point.y;
+    } else if(line.slope === 0) {
+      x = point.x;
+      y = line.y;
+    } else {
 
-    //Find x value
-    const xSlopes = line.slope - perpSlope;
-    const xIntercepts = perpIntercept - line.intercept;
-    const x = xIntercepts / xSlopes;
 
-    const y = this.yPosOnLine(line, x);
+      const perpSlope = (1 / line.slope) * -1;
+      const perpIntercept = this.calculateIntercept(perpSlope, point);
+      const perpLine = { slope: perpSlope, intercept: perpIntercept };
+
+      //Find x value
+      const xSlopes = line.slope - perpSlope;
+      const xIntercepts = perpIntercept - line.intercept;
+      x = xIntercepts / xSlopes;
+
+      y = this.yPosOnLine(line, x);
+    }
 
     return {x,y};
   }

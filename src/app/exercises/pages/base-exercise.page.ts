8f3abaf560bade8782c2c8e39ae6f3c95c64f0a7
Adam Weeks <aoweeks@gmail.com>
2021-06-20 16:52:16 +0100

    Add body-scroll-lock to disable page bounce on iOS

--- src/app/exercises/pages/base-exercise.page.ts
+++ src/app/exercises/pages/base-exercise.page.ts
@@ -1,12 +1,18 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { Base2dCanvasComponent } from '../2d-canvases/base-2d-canvas.component';
+
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks
+} from 'body-scroll-lock';
 
 @Component({
   selector: 'app-base-exercise',
   templateUrl: './base-exercise.page.html',
   styleUrls: ['./base-exercise.page.scss'],
 })
-export class BaseExercisePage {
+export class BaseExercisePage implements AfterViewInit{
 
   @ViewChild('drawingCanvas') drawingCanvasCmp: Base2dCanvasComponent;
 
@@ -19,6 +25,10 @@ export class BaseExercisePage {
   private undoable = true;
 
   constructor() { }
+
+  ngAfterViewInit() {
+    disableBodyScroll(this.drawingCanvasCmp);
+  }
 
  /**
   * OUTPUT EVENT HANDLERS

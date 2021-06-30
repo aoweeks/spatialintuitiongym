078f47edd29bf0c2aaeff7c026e3e12aa746ce11
Adam Weeks <aoweeks@gmail.com>
2021-06-30 23:44:23 +0100

    Change volume icon based on volume value

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -10,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
+  public volume = 10;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public getVolumeIcon(): string {
+
+    let iconName = 'volume-';
+
+    switch (this.volume) {
+
+      case 0:
+        iconName += 'off';
+        break;
+      case 1:
+      case 2:
+      case 3:
+        iconName += 'low';
+        break;
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+        iconName += 'medium';
+        break;
+      case 8:
+      case 9:
+      case 10:
+        iconName += 'high';
+    }
+
+    iconName += '-outline';
+
+    return iconName;
   }
 
 }

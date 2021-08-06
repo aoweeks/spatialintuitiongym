1d51f192af20964e07bfd137315b6cfcb07abbbf
Adam Weeks <aoweeks@gmail.com>
2021-08-06 18:23:27 +0100

    Flesh out exercises model

--- src/app/models/exercise.model.ts
+++ src/app/models/exercise.model.ts
@@ -1,12 +1,20 @@
 export class Exercise {
 
+  private name: string;
+  private projection: string;
+
   private highScore: number;
   private scoreUnit: string;
 
   constructor(
+    name: string,
+    projection: string,
     highScore: number,
-    scoreUnit: string
+    scoreUnit: string,
   ) {
-
+    this.name = name;
+    this.projection = projection;
+    this.highScore = highScore;
+    this.scoreUnit = scoreUnit;
   }
 }
